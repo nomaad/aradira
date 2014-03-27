e3dda728b99f24350fc46cc9e179eb159d78e50e
Matthias Zaugg <info@nomaad.ch>
2014-03-27 21:43:56 +0100

    Flat-UI and Leaflet added

--- public/modules/core/controllers/home.js
+++ public/modules/core/controllers/home.js
@@ -2,4 +2,30 @@
 
 angular.module('mean.core').controller('HomeController', ['$scope', 'Authentication', function ($scope, Authentication) {
     $scope.authentication = Authentication;
+
+    angular.extend($scope, {
+        // set up map center
+        cen: {
+            lat: 46.946732,
+            lng: 7.44873,
+            zoom: 13
+        },
+    });
+
+    angular.extend($scope, {
+        paths: {
+            c1: {
+                weight: 2,
+                color: '#ff612f',
+                latlngs: {
+                    lat: 46.946732,
+                    lng: 7.44873,
+                    draggable: false
+                },
+                radius: 200,
+                type: 'circle'
+            },
+        }
+    });
+
 }]);
