42aefc033ee22a2eba572b82465a0425215cde22
Matthias Zaugg <info@nomaad.ch>
2014-03-28 14:29:50 +0100

    Footer and general layout

--- public/modules/core/controllers/home.js
+++ public/modules/core/controllers/home.js
@@ -2,21 +2,17 @@
 
 angular.module('mean.core').controller('HomeController', ['$scope', 'Authentication', 'geolocation', 'leafletData', function ($scope, Authentication, geolocation, leafletData) {
     $scope.authentication = Authentication;
-    var map = leafletData.getMap();
-    geolocation.getLocation().then(function(data){
+
+    /*geolocation.getLocation().then(function(data){
         $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
-        /*$scope.cen = {
-            lat: data.coords.latitude,//46.946732,
-            lng: data.coords.longitude, //7.44873,
-            zoom: 13
-        };*/
+
         leafletData.getMap().then(function(map) {
             map.setView({
                 lat: data.coords.latitude,//46.946732,
                 lng: data.coords.longitude, //7.44873,
             },12);
         });
-    });
+    });*/
 
     angular.extend($scope, {
         // set up map center
