0233084308e4c74ec544fed7929899f43d335b15
Matthias Zaugg <info@nomaad.ch>
2014-04-19 11:47:55 +0200

    Cleanup

--- public/modules/offers/config/routes.js
+++ public/modules/offers/config/routes.js
@@ -5,75 +5,32 @@ angular.module('aradira.offers').config(['$stateProvider',
 	function($stateProvider) {
 		// Offers state routing
 		$stateProvider.
-
 		state('createOffer', {
 			url: '/angebote/neu',
 			templateUrl: 'modules/offers/views/create.html'
 		}).
-		/*state('viewOffer', {
-			url: '/angebote/:offerId',
-			templateUrl: 'modules/offers/views/view.html'
-		}).*/
 		state('editOffer', {
 			url: '/angebote/:offerId/bearbeiten',
 			templateUrl: 'modules/offers/views/edit.html'
 		}).
-            state('listOffers', {
-                url: '/angebote',
-                templateUrl: 'modules/offers/views/list.html'
-            }).
-            /*state('listOffers.details', {
-             url: '/:offerId',
-
-             onEnter: function($stateParams, $state, $modal) {
-                 console.log($modal);
-                 $modal.open({templateUrl: 'modules/offers/views/view.html'});
-             }
-             })*/
-        state("listOffers.details", {
-                url: "/:offerId",
-                onEnter: function($stateParams, $state, $modal, $resource) {
-                    console.log($stateParams, $state);
-                    $modal.open({
-                        templateUrl: 'offerDetails.html',
-                        backdrop: 'static',
-                        controller: 'OfferDetailsController',
-                        windowClass: 'details-dialog'
-                    }).result.then(function(result) {
-                            if (result) {
-                                return $state.transitionTo("listOffers");
-                            }
-                        });
-                }
-            });
-        /*state('listOffers.details', {
-            url: '/angebote/:offerId',
-            onEnter: function($stateParams, $state, $modal, Offers, $resource) {
+        state('listOffers', {
+           url: '/angebote',
+           templateUrl: 'modules/offers/views/list.html'
+       }).
+        state('listOffers.details', {
+            url: '/:offerId',
+            onEnter: function($stateParams, $state, $modal) {
                 $modal.open({
-                    templateUrl: 'modules/offers/views/view.html',
-                    /*resolve: {
-                        offer: function() { Offers.get({
-                            offerId: $stateParams.offerId
-                        }); }
-                    },
-                    controller: OffersController, /*['$scope', 'offer', function($scope, offer) {
-                        $scope.offer = offer;
-                        /*$scope.dismiss = function() {
-                            $scope.$dismiss();
-                        };
-
-                        $scope.save = function() {
-                            item.update().then(function() {
-                                $scope.$close(true);
-                            });
-                        };
-                    }]*//*
+                    templateUrl: 'offerDetails.html',
+                    backdrop: 'static',
+                    controller: 'OfferDetailsController',
+                    windowClass: 'details-dialog'
                 }).result.then(function(result) {
                         if (result) {
                             return $state.transitionTo("listOffers");
                         }
                     });
             }
-        });*/
+        });
 	}
 ]);
